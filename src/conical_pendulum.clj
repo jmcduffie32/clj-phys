02d28730b3ccfbddd668eb46702433f43763e7b0
Jon McDuffie <bobber321988@gmail.com>
2020-09-29 20:42:42 -0400

    add 3rd dimension to concical pendulum plot

--- src/conical_pendulum.clj
+++ src/conical_pendulum.clj
@@ -5,30 +5,26 @@
 (def g 9.81)
 
 (defn create-points
-  [{:keys [l
-           theta0
+  [{:keys [l omega
            t0 tf dt]}]
-  (let [omega (Math/sqrt (/ g l))
-        period (/ (* 2 Math/PI) omega)]
+  (let [period (/ (* 2 Math/PI) omega)
+        cos_theta (/ g (* omega omega l))
+        sin_theta (Math/sqrt (- 1 (* cos_theta cos_theta)))
+        z (/ (* -1 g) (* omega omega))
+        vz 0
+        r (/ (* sin_theta g) (* omega omega cos_theta))]
     (for [t (range t0 tf dt)]
-      (let [theta (* theta0 omega (Math/cos (* omega (- t t0))))
-            dtheta-dt (* -1 omega theta0 (Math/sin (* omega (- t t0))))
-            x (* l (Math/sin theta))
-            y (* -1 l (Math/cos theta))
-            vx (* l dtheta-dt (Math/cos theta))
-            vy (* l dtheta-dt (Math/sin theta))]
-        [t x y vx vy theta dtheta-dt]))))
+      (let [x (* r (Math/cos (* omega t)))
+            y (* r (Math/sin (* omega t)))
+            vx (* -1 r (Math/sin (* omega t)) omega)
+            vy (* r (Math/cos (* omega t)) omega)]
+        [t x y z vx vy vz]))))
 
 (def initial-values {:l 1
-                     :theta0 0.314
+                     :omega 6.28
                      :t0 0
-                     :tf 20
+                     :tf 10
                      :dt 0.01})
-
-(def gnuplot-script "
-set title 'Conical Pendulum';
-plot 'conical_pendulum.dat' using 2:3 w l t 'y(x)';
-")
 
 (defn create-script [{:keys [path animate]}]
   (if animate
@@ -36,18 +32,21 @@ plot 'conical_pendulum.dat' using 2:3 w l t 'y(x)';
 set title 'Conical Pendulum';
 set xrange [{{x-min}}:{{x-max}}];
 set yrange [{{y-min}}:{{y-max}}];
+set zrange [{{z-min}}:{{z-max}}];
 do for [ii=1:{{iterations}}] {
-  plot 'conical_pendulum.dat' every ::1::ii using 2:3 w l ls 1 notitle,
- 'conical_pendulum.dat' every ::ii::ii using 2:3 w p pt 7 ps 4 notitle;
+  splot 'conical_pendulum.dat' every ::1::ii using 2:3:4 w l ls 1 notitle,
+  'conical_pendulum.dat' every ::ii::ii using 2:3:4 w p pt 7 ps 4 notitle;
 }
 " {:x-min (nth (apply min-key #(nth % 1) path) 1)
    :x-max (nth (apply max-key #(nth % 1) path) 1)
    :y-min (nth (apply min-key #(nth % 2) path) 2)
    :y-max (nth (apply max-key #(nth % 2) path) 2)
+   :z-min -0.5 ;;(nth (apply min-key #(nth % 3) path) 3)
+   :z-max 0 ;;(nth (apply max-key #(nth % 3) path) 3)
    :iterations (- (count path) 1)} )
     (str "
 set title 'Conical Pendulum';
-plot 'conical_pendulum.dat' using 2:3 w l t 'y(x)';
+splot 'conical_pendulum.dat' using 2:3:4 w l t 'y(x)';
 ")))
 
 
